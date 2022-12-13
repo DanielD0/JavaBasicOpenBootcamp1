0cfda7fdb573bba11762a0352bc8e715b59cc63d
Daniel Diaz <37585619+DanielD0@users.noreply.github.com>
2022-12-13 10:40:34 -0600

    Agregar archivos del ejercicio

---
+++ src/com/example/AdiosMundo.java
@@ -0,0 +1,24 @@
+package com.example;
+
+/**
+ * Clase para crear un hola mundo basico en Java
+ */
+public class AdiosMundo {
+    /**
+     * Punto de entrada a la aplicacion Java
+     * Permite ejecutar la aplicacion
+     * @param args - argumentos de entrada para la aplicacion
+     */
+
+    public static void main(String[] args) {
+        //esto es un comentario
+        System.out.println("Adios mundo cruel!"); //esto es otro comentario
+
+        /*
+         Esto es un comentario multilinea:
+         1- Crear funcion suma
+         2- Crear variables tipo numerico
+         3- Utilizat la funcion suma para calcular la suma de los numeros
+        */
+    }
+}
---
+++ src/com/example/Tipos.java
@@ -0,0 +1,30 @@
+package com.example;
+
+public class Tipos {
+    public static void main(String[] args) {
+        //1.numericos
+        //1.1 enteros
+            byte variable1 = 4;
+                System.out.println(variable1);
+            short variable2 = 10;
+                System.out.println(variable2);
+            int variable3 = 30;
+                System.out.println(variable3);
+            long variable4 = 100;
+                System.out.println(variable4);
+        //1.2 decimales
+            float variable5 = 5.5f;
+                System.out.println(variable5);
+            double variable6 = 10.5d;
+                System.out.println(variable6);
+        // 2. boleano
+            boolean variable7 = false;
+        System.out.println(variable7);
+        // 3.texto
+            String variable8 = "mi nombre";
+             System.out.println(variable8);
+            char variable9 = 'A';
+             System.out.println(variable9);
+
+    }
+}
